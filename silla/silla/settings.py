da96febd7475ee66d3d4707d9ae6b68c61d8b450
unknown <diosii@gmail.com>
2014-02-22 01:22:11 +0100

    Añadidos esqueletos de aplicaciones para la web

--- silla/silla/settings.py
+++ silla/silla/settings.py
@@ -65,7 +65,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
@@ -78,5 +78,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR) + '../static/'
 
 STATIC_URL = '/static/'
